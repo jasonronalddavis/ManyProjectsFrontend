75db08439cbed2bdcdf270d2081d37eb7d0af5ee
jasonronalddavis <jdavis@c.ringling.edu>
2021-09-11 21:34:47 -0500

    added some stuff

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("success");
+const endPoint = "http://localhost:3000/api/v1";
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    getUser()
+})
+
+
+function getUser() {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(category => {
+
+category.data.forEach
+
+
+
+
+
+
+
+
+
+
+
+    console.log(category);  
+    })
+  }
+  
