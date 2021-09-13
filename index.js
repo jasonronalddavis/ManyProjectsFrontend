1a2f2eb8857d7ebaca390d6de203f1c552a0d69b
jasonronalddavis <jdavis@c.ringling.edu>
2021-09-12 20:31:42 -0500

    more frontend stuff

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-const endPoint = "http://localhost:3000/api/v1";
+const endPoint = "http://localhost:3000/api/v1"
 
 
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    getUser()
+    getCategory()
 })
 
 
-function getUser() {
+function getCategory() {
     fetch(endPoint)
     .then(response => response.json())
-    .then(category => {
-
-category.data.forEach
-
-
-
-
-
-
-
-
-
-
-
-    console.log(category);  
-    })
-  }
-  
+    .then( categories => {
+categories.data.forEach(category => {
+const categoryMarkup = `
+    <div data-id=${category.id}>
+    <img src="assets/images/PaintPallete.png" height="100" width="100">
+        <h3>${category.attributes.name}</h3>
+        <button data-id=${category.id}>edit</button>
+        </div>
+    <br><br>`;
+        document.querySelector('#category-container').innerHTML+=
+        categoryMarkup
+         }) 
+     }) 
+   
+    }
